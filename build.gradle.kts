a34acbc5fd91c3d842a03fc02fec552cce7607b7
darksaid98 <darksaid9889@gmail.com>
2023-08-03 18:20:39 +0200

    feat: debugger setup and info

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,6 +83,11 @@ tasks {
     runServer {
         // Configure the Minecraft version for our task.
         minecraftVersion("1.20.1")
+
+        // IntelliJ IDEA debugger setup: https://docs.papermc.io/paper/dev/debugging#using-a-remote-debugger
+        jvmArgs("-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005")
+        systemProperty("terminal.jline", false)
+        systemProperty("terminal.ansi", true)
     }
 }
 
