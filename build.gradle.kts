f762a2a849183f3f492a3b5a0157d4d8b92ac18b
darksaid98 <darksaid9889@gmail.com>
2023-08-03 18:21:03 +0200

    tweak: more verbose compile time logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,7 @@ tasks {
         // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
         // See https://openjdk.java.net/jeps/247 for more information.
         options.release.set(17)
-        options.compilerArgs.add("-Xlint:-deprecation")
+        options.compilerArgs.addAll(arrayListOf("-Xlint:all", "-Xlint:-processing", "-Xdiags:verbose"))
     }
 
     processResources {
