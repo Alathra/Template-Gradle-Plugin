163b49ad27a65379811e635893482deff2767806
darksaid98 <darksaid9889@gmail.com>
2023-11-07 13:40:46 +0100

    feat: foojay resolver gradle plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,12 @@
 pluginManagement {
     repositories {
         gradlePluginPortal()
+        mavenCentral()
     }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.7.0")
 }
 
 rootProject.name = "ExamplePlugin"
