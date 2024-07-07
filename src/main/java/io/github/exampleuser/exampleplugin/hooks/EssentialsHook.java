84f0ebaa3d6ff31041b49dcc80b200a52aba41fa
darksaid98 <darksaid9889@gmail.com>
2024-07-07 22:57:27 +0200

    feat: plugin hook example

---
+++ src/main/java/io/github/exampleuser/exampleplugin/hooks/EssentialsHook.java
@@ -0,0 +1,11 @@
+package io.github.exampleuser.exampleplugin.hooks;
+// Example of how to hook into EssentialsX. Then make an accessor for the hook in ExamplePlugin, like ExamplePlugin#getEssentialsHook
+/*
+import com.earth2me.essentials.Essentials;
+
+public class EssentialsHook extends AbstractPluginHook<Essentials> {
+    public EssentialsHook() {
+        super("Essentials");
+    }
+}
+*/
