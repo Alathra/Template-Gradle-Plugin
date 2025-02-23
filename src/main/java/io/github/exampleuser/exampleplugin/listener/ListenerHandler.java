3c796847a31cb4e40d296c0a2d43604d98b53e96
darksaid98 <darksaid9889@gmail.com>
2025-02-23 03:08:09 +0100

    feat: add listener list

--- src/main/java/io/github/exampleuser/exampleplugin/listener/ListenerHandler.java
+++ src/main/java/io/github/exampleuser/exampleplugin/listener/ListenerHandler.java
@@ -2,12 +2,17 @@ package io.github.exampleuser.exampleplugin.listener;
 
 import io.github.exampleuser.exampleplugin.ExamplePlugin;
 import io.github.exampleuser.exampleplugin.Reloadable;
+import org.bukkit.event.Listener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A class to handle registration of event listeners.
  */
 public class ListenerHandler implements Reloadable {
     private final ExamplePlugin plugin;
+    private final List<Listener> listeners = new ArrayList<>();
 
     /**
      * Instantiates a the Listener handler.
@@ -24,8 +29,13 @@ public class ListenerHandler implements Reloadable {
 
     @Override
     public void onEnable(ExamplePlugin plugin) {
+        listeners.clear(); // Clear the list to avoid duplicate listeners when reloading the plugin
+//        listeners.add(new ExampleListener());
+
         // Register listeners here
-        //plugin.getServer().getPluginManager().registerEvents(new PlayerJoinListener(plugin), plugin);
+        for (Listener listener : listeners) {
+            plugin.getServer().getPluginManager().registerEvents(listener, plugin);
+        }
     }
 
     @Override
