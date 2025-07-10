c8db8f5d74fca6fb84b644e9285ba7919114f5b7
darksaid98 <darksaid9889@gmail.com>
2025-07-10 11:15:52 +0200

    fix(db): broken migration for mysql

--- src/main/resources/db/migration/V1__base_tables.sql
+++ src/main/resources/db/migration/V1__base_tables.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS "${tablePrefix}sync" (
 -- Table for storing player cooldowns
 CREATE TABLE IF NOT EXISTS "${tablePrefix}cooldowns" (
     "uuid" BINARY(16) NOT NULL,
-    "cooldown_type" TEXT NOT NULL,
+    "cooldown_type" VARCHAR(255) NOT NULL,
     "cooldown_time" TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     UNIQUE KEY "${tablePrefix}unique_uuid_type" ("uuid", "cooldown_type")
 );
